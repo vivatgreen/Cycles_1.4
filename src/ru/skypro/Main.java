8414390a5f786f19f3dbd4929dd704ea713a4f8e
vivatgreen <grindsasha@bk.ru>
2022-03-13 18:22:28 +0300

    Коммит

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -7,23 +7,26 @@ public class Main {
 
     public static void main(String[] args) {
         // Задача №1
-        int a = 1;
-        while (a <= 10) {
-            System.out.println(a++);
+        int a = 0;
+        while (a < 10) {
+            System.out.print(++ a + " ");
         }
         System.out.println();
 
 
-        for (int i = 10; i > 0; i--) {
-            System.out.println(i);
+        for (; a > 0; a--) {
+            System.out.print(a + " ");
         }
+        System.out.println();
+
         // Задача №2
         for (int dayNumber = 4; dayNumber <= 31; dayNumber++) {
             if (dayNumber % 7 == 4) {
                 System.out.println("Сегодня пятница " + dayNumber + "-е число. Необходимо подготовить отчет.");
-
             }
         }
+        System.out.println();
+
         // Задача №3
         int currentYear = 2022;
         int start = currentYear - 200;
